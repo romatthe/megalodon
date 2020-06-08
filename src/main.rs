8979acd701938e3811f16b576473af6abbce6267
Robin Mattheussen <robin.mattheussen@gmail.com>
2020-06-08 16:05:40 +0200

    Run actix-rt inside of tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, Responder, web};
+
+async fn hello_world() -> impl Responder {
+    "Hello World!"
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // Actix requires spawning its own single-threaded runtime. Here we use tokio to explicitly spawn
+    // an Actix system inside of it. This way we can still use tokio::spawn when we need it for other libraries
+    let local = tokio::task::LocalSet::new();
+    let sys = actix_rt::System::run_in_tokio("system", &local);
+    let server = HttpServer::new(|| App::new().route("/", web::get().to(hello_world)))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await?;
+
+    sys.await?;
+
+    Ok(server)
 }
